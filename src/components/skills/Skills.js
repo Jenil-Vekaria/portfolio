3169a33251130703f69393450cde4b61b5f0af6b
Jenil-Vekaria <jvekaria@ryerson.ca>
2022-04-28 16:59:53 -0400

    Add skills section and fix css issues

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -6,7 +6,7 @@ const Skills = () => {
         <section className='container' id="skills">
             <div className='section-header'>
                 <h5>What Skills I Have</h5>
-                <h2>My Experience</h2>
+                <h2>Technical Skills</h2>
             </div>
             <div className='skills-container'>
                 <div className='skills-card'>
@@ -96,24 +96,6 @@ const Skills = () => {
                 </div>
 
                 <div className='skills-card'>
-                    <h3 className='skills-title text-color-primary'>CI/CD</h3>
-                    <div className='skills-list'>
-                        <article className='skills-item'>
-                            <BsPatchCheckFill className="skills-item-icon" />
-                            <h5>Ullamco</h5>
-                        </article>
-                        <article className='skills-item'>
-                            <BsPatchCheckFill className="skills-item-icon" />
-                            <h5>Lorem</h5>
-                        </article>
-                        <article className='skills-item'>
-                            <BsPatchCheckFill className="skills-item-icon" />
-                            <h5>Cupidatat</h5>
-                        </article>
-                    </div>
-                </div>
-
-                <div className='skills-card'>
                     <h3 className='skills-title text-color-primary'>Tools</h3>
                     <div className='skills-list'>
                         <article className='skills-item'>
@@ -139,6 +121,10 @@ const Skills = () => {
                         <article className='skills-item'>
                             <BsPatchCheckFill className="skills-item-icon" />
                             <h5>Quis</h5>
+                        </article>
+                        <article className='skills-item'>
+                            <BsPatchCheckFill className="skills-item-icon" />
+                            <h5>Tempor</h5>
                         </article>
                         <article className='skills-item'>
                             <BsPatchCheckFill className="skills-item-icon" />
